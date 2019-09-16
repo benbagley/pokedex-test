398d1076f1603142bac58973441b6d60a8540ad7
Ben Bagley <hello@benbagley.dev>
2019-09-17 00:10:24 +0100

    2.3

--- scripts.js
+++ scripts.js
@@ -13,5 +13,18 @@ var respository = [
     name: 'Squirtle',
     height: '1.08',
     types: ['Water']
+  },
+  {
+    name: 'Caterpie',
+    height: '1.00',
+    types: ['Bug']
   }
 ]
+
+for (var i = 0; i < respository.length; i++) {
+  document.write('<br />', 'Name: ' + respository[i].name, ' height: ' + respository[i].height);
+
+  if (i < respository.length && respository[i].height > 1.00) {
+    document.write(' - wow that\'s big');
+  }
+}
