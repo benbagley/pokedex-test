030e4a226e2630f5c29439dc44476afd50f1fba9
Ben Bagley <hello@benbagley.dev>
2019-09-16 22:56:06 +0100

    2.1

---
+++ scripts.js
@@ -0,0 +1,17 @@
+var respository = [
+  {
+    name: 'Bulbasaur',
+    height: '2.04',
+    types: ['Grass', 'Poison']
+  },
+  {
+    name: 'Charmander',
+    height: '2.00',
+    types: ['Fire']
+  },
+  {
+    name: 'Squirtle',
+    height: '1.08',
+    types: ['Water']
+  }
+]
